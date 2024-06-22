a104edf9bf2fd3fad4163aefcdec76c33c1ac3d3
ihzarf <ihzarestu01@gmail.com>
2024-06-22 12:29:49 +0700

    push

---
+++ kawan_app/src/app/courses/courses.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Course } from './course.model';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.page.html',
+  styleUrls: ['./courses.page.scss'],
+})
+export class CoursesPage implements OnInit {
+  courses$: Observable<Course[]>;
+
+  constructor(
+    private coursesService: CoursesService,
+    private loadingCtrl: LoadingController,
+  ) {}
+
+  async ngOnInit() {
+    const loading = await this.loadingCtrl.create({message: 'Loading...'});
+    loading.present();
+
+    this.courses$ = this.coursesService.getCourses().pipe(
+      tap(courses => {
+        loading.dismiss();
+        return courses;
+      })
+    );
+  }
+
+}
