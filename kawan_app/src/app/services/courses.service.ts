13f44b872a3f11230f73310971b367a59840c0f4
ihzarf <ihzarestu01@gmail.com>
2024-06-22 13:00:43 +0700

    push

---
+++ kawan_app/src/app/services/courses.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Course } from '../courses/course.model';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CoursesService {
+  apiUrl = 'http://localhost:8000/api';
+
+  constructor(private http: HttpClient) {}
+
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>('${this.apiUrl}/courses');
+  }
+}
